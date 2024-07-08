df207ba088fd0e2c4a5a7a6b31677359778938d4
DarkxSurfer <97832544+DarkxSurfer@users.noreply.github.com>
2024-07-08 17:04:05 +0500

    Update textformwidget.dart

--- lib/Widgets/textformwidget.dart
+++ lib/Widgets/textformwidget.dart
@@ -10,15 +10,7 @@ class TextFormWidget extends StatelessWidget {
                           decoration: const InputDecoration(
                             contentPadding: EdgeInsets.only(
                                 bottom: 10, top: 10, left: 10, right: 10),
-                            filled: true,
-                            fillColor: Colors.white,
-                            hintStyle: TextStyle(color: Colors.green),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
-                            ),
+                            
                           ),
                           cursorColor: Colors.black,
                           style: const TextStyle(color: Colors.black),
